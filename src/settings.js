f066e3b67b8ba077adfb899facebb5ca5f6692e7
chameleon-fzb <1466518303@qq.com>
2022-05-31 16:33:02 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'SPH_Admin', // 网页标题
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false, // 控制nav头部是否固定在视口上方
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false // 控制侧边栏logo是否显示
+}
