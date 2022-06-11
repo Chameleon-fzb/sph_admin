c9a688fc53fae0218d0bcd6bab360aefbe1649e8
chameleon-fzb <1466518303@qq.com>
2022-06-11 20:50:27 +0800

    封装可提示按钮

---
+++ src/components/HintBtn/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-tooltip
+    :content="title"
+    placement="top"
+  >
+    <el-button
+      v-bind="$attrs"
+      v-on="$listeners"
+    />
+  </el-tooltip>
+</template>
+<script>
+export default {
+  name: 'HintBtn',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
